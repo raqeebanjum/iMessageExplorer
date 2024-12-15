c6327f737eb291138b9512a91a11715632c8fc19
Raqeeb Anjum <rraqeebanjum04@gmail.com>
2024-12-14 22:44:23 -0600

    Created Project

---
+++ iMessageExplorer/iMessageExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMessageExplorerApp.swift
+//  iMessageExplorer
+//
+//  Created by Raqeeb Anjum on 12/14/24.
+//
+
+import SwiftUI
+
+@main
+struct iMessageExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iMessageExplorerTests/iMessageExplorerTests.swift
@@ -0,0 +1,17 @@
+//
+//  iMessageExplorerTests.swift
+//  iMessageExplorerTests
+//
+//  Created by Raqeeb Anjum on 12/14/24.
+//
+
+import Testing
+@testable import iMessageExplorer
+
+struct iMessageExplorerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
